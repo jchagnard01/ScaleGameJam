40e772a33ce3dc7942d4dcdca2d971cf14f5c201
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2026-07-18 07:44:27 -0500

    Created AnimatorManager.cs

---
+++ JacobsStory/Assets/KScripts/AnimatorManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    public void UpdateAnimatorValue(float horizontalMovement, float verticalMovement)
+    {
+        
+    }
+}
